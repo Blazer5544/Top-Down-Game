f3c00a61b1569deb9b338e28743118d7a3a7d74b
Blazer5544 <javiermorales2075@gmail.com>
2024-10-01 11:49:13 -0400

    Set up sprite and animation while doing the transition between scenes

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -1,29 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
 
-    private float Move;
-    private float TopDown;
+    //private float Move;
+    //private float TopDown;
 
     public float speed;
+
+    public Sprite upSprite;
+    public Sprite leftSprite;
+    public Sprite rightSprite;
+    public Sprite downSprite;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move = Input.GetAxisRaw("Horizontal");
-        TopDown = Input.GetAxisRaw("Vertical");
+        Vector3 newPosition = transform.position;
 
-        rb.velocity = new Vector2(TopDown * speed, rb.velocity.y);
-        rb.velocity = new Vector2(Move * speed, rb.velocity.x);
+        if (Input.GetKey("w"))
+        {
+            newPosition.y += speed;
+            sr.sprite = upSprite;
+        }
+
+        if (Input.GetKey("a"))
+        {
+            newPosition.x -= speed;
+            sr.sprite = leftSprite;
+        }
+
+        if (Input.GetKey("s"))
+        {
+            newPosition.y -= speed;
+            sr.sprite = downSprite;
+        }
+
+        if (Input.GetKey("d"))
+        {
+            newPosition.x += speed;
+            sr.sprite = rightSprite;
+        }
+
+        transform.position = newPosition;
+
+        //Move = Input.GetAxisRaw("Horizontal");
+        //TopDown = Input.GetAxisRaw("Vertical");
+
+        //rb.velocity = new Vector2(TopDown * speed, rb.velocity.y);
+        //rb.velocity = new Vector2(Move * speed, rb.velocity.x);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("door0"))
+        {
+            Debug.Log("change scene");
+            SceneManager.LoadScene(1);
+        }
     }
 }
